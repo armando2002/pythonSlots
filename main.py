5bf9c0cf5ece82fd8931ab23a379df165f66d735
Stephen <armando2002@gmail.com>
2022-12-19 14:48:03 -0800

    added balance check and finished bug fixes

--- main.py
+++ main.py
@@ -1,8 +1,83 @@
+import random
+
 # sets max lines constant for bars
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+# set values for slot machine (bars/rows)
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 3,
+    "C": 6,
+    "D": 8
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+# check what player won
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            # if symbol doesn't match, no points
+            if symbol != symbol_to_check:
+                break
+        # else add the value based on bet to winnings
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
+# takes 3 parameters from slot machine and spins
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        # _ = anonymous variable
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    # for each column, generate rows
+    for col in range(cols):
+        column = []
+        # copy all_symbols into current_symbols
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        # print new line
+        print()
 
 
 # set initial deposit
@@ -52,9 +127,7 @@ def get_bet():
     return amount
 
 
-# main function, restarts game
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
 
     # get bet and check that bet is within balance
@@ -68,6 +141,29 @@ def main():
         else:
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}")
+    # pass every single line from winning lines list
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+
+# main function, restarts game
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is: ${balance}")
+        answer = input("Press enter to play (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
+# TODO: Add catch for zero balance to allow game to end
 
 
 main()
